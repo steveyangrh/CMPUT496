4015328d49ba67a38396f5af609728f953ca4147
asampley <sampley@ualberta.ca>
2017-03-21 14:56:17 -0600

    Added niceness to test scripts

--- Assignments/Assignment3/Go3vsGo4AC.sh
+++ Assignments/Assignment3/Go3vsGo4AC.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gogui-twogtp -size 5 -komi 6.5 -games 100 -black "Go3/Go3.py --sim 50" -white "Go4AC/Go4.py --sim 50" -sgffile test1 -auto -threads 8 -alternate
+nice -n 19 gogui-twogtp -size 5 -komi 6.5 -games 100 -black "Go3/Go3.py --sim 50" -white "Go4AC/Go4.py --sim 50" -sgffile test1 -auto -threads 8 -alternate

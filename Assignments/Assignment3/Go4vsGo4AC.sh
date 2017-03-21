934080facb44190e46505825017b42a70f9e9b1e
asampley <sampley@ualberta.ca>
2017-03-21 15:20:44 -0600

    Renamed 'Go4 Origin' to 'Go4Origin' to make scripts easier

--- Assignments/Assignment3/Go4vsGo4AC.sh
+++ Assignments/Assignment3/Go4vsGo4AC.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nice -n 19 gogui-twogtp -size 5 -komi 6.5 -games 100 -black "Go4 Origin/Go4.py --sim 50" -white "Go4AC/Go4.py --sim 50" -sgffile test3 -auto -threads 8 -alternate
+nice -n 19 gogui-twogtp -size 5 -komi 6.5 -games 100 -black "Go4Origin/Go4.py --sim 50" -white "Go4AC/Go4.py --sim 50" -sgffile test3 -auto -threads 8 -alternate
--- Assignments/Assignment3/Go4vsGo4ACAD.sh
+++ Assignments/Assignment3/Go4vsGo4ACAD.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nice -n 19 gogui-twogtp -size 5 -komi 6.5 -games 100 -black "Go4 Origin/Go4.py --sim 50" -white "Go4ACAD/Go4.py --sim 50" -sgffile test4 -auto -threads 8 -alternate
+nice -n 19 gogui-twogtp -size 5 -komi 6.5 -games 100 -black "Go4Origin/Go4.py --sim 50" -white "Go4ACAD/Go4.py --sim 50" -sgffile test4 -auto -threads 8 -alternate

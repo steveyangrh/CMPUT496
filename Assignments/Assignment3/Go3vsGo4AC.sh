26d432e6e6b76f5bff48b67480f66f09a42380a2
asampley <sampley@ualberta.ca>
2017-03-21 13:34:51 -0600

    Added komi to test scripts

--- Assignments/Assignment3/Go3vsGo4AC.sh
+++ Assignments/Assignment3/Go3vsGo4AC.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gogui-twogtp -size 5 -games 100 -black "Go3/Go3.py --sim 50" -white "Go4/Go4AC.py --sim 50" -sgffile test1 -auto -thread 8 -alternate
+gogui-twogtp -size 5 -komi 6.5 -games 100 -black "Go3/Go3.py --sim 50" -white "Go4/Go4AC.py --sim 50" -sgffile test1 -auto -thread 8 -alternate

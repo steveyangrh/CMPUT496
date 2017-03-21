378bc660f6c267274a914661009247d27719d942
asampley <sampley@ualberta.ca>
2017-03-21 14:51:38 -0600

    Fixed incorrect flag for arguments in test scripts

--- Assignments/Assignment3/Go3vsGo4ACAD.sh
+++ Assignments/Assignment3/Go3vsGo4ACAD.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gogui-twogtp -size 5 -komi 6.5 -games 100 -black "Go3/Go3.py --sim 50" -white "Go4/Go4ACAD.py --sim 50" -sgffile test2 -auto -thread 8 -alternate
+gogui-twogtp -size 5 -komi 6.5 -games 100 -black "Go3/Go3.py --sim 50" -white "Go4ACAD/Go4.py --sim 50" -sgffile test2 -auto -threads 8 -alternate
--- Assignments/Assignment3/Go4vsGo4AC.sh
+++ Assignments/Assignment3/Go4vsGo4AC.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gogui-twogtp -size 5 -komi 6.5 -games 100 -black "Go4/Go4.py --sim 50" -white "Go4/Go4AC.py --sim 50" -sgffile test3 -auto -thread 8 -alternate
+gogui-twogtp -size 5 -komi 6.5 -games 100 -black "Go4 Origin/Go4.py --sim 50" -white "Go4AC/Go4.py --sim 50" -sgffile test3 -auto -threads 8 -alternate
